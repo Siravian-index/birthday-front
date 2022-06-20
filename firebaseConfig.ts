53d7f57640916aa8443ae4738ae0cba514d6be06
David <depch47@gmail.com>
2022-06-20 17:22:15 -0500

    Deployed

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAovMPtSvt7rvVF6QJduHhjoZAXPNmSdPc",
+    authDomain: "sofka-birthday-tracker.firebaseapp.com",
+    projectId: "sofka-birthday-tracker",
+    storageBucket: "sofka-birthday-tracker.appspot.com",
+    messagingSenderId: "243045668874",
+    appId: "1:243045668874:web:b10358f71de950147be7f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
