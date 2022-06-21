e457502a60e8ec58a6fa0c1f17ba22c7af1dfbed
David <depch47@gmail.com>
2022-06-20 20:58:28 -0500

    working on page not found component

--- src/components/error/PageNotFound.tsx
+++ src/components/error/PageNotFound.tsx
@@ -1,9 +1,24 @@
 import * as React from "react"
+import {Button, Container, Text, Title} from "@mantine/core";
+import {useNavigate} from "react-router-dom";
+import {ArrowBack} from "tabler-icons-react";
 
-interface IProps {}
+interface IProps {
+}
 
-const PageNotFound : React.FC<IProps> = () => {
-    return <>TO-do implement not found page</>
+const PageNotFound: React.FC<IProps> = () => {
+    const navigate = useNavigate()
+
+    return <Container>
+        <Title order={1} align='center'>404</Title>
+        <Text align='center'>Not found</Text>
+        <Button
+            leftIcon={<ArrowBack/>}
+            onClick={() => navigate("/")}
+            variant="gradient"
+            gradient={{from: 'indigo', to: 'cyan'}}
+        >Go back</Button>
+    </Container>
 }
 
 export default PageNotFound
